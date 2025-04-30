526b7b7138742c4ff718f4243cd81d96a02eada4
Mohammad Hasrat <hasrat233222@gmail.com>
2025-04-30 16:13:13 +0530

    jwt for seller is created

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -46,3 +46,27 @@ export const isUserLogedIn = async (req, res, next) => {
         throw new ApiError(401, error.message)
     }
 }
+
+
+
+export const isSellerLogedIn = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization;
+        if (!token) {
+            throw new ApiError(403, "Token not found!!!")
+        }
+
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+            if (error) {
+                throw new ApiError(401, "Unauthorized User")
+            }
+
+            req.sellerId = decoded.sellerId
+
+            next()
+
+        })
+    } catch (error) {
+        throw new ApiError(401, error.message)
+    }
+}
